b51c1685d5243e8e7814961fa54e881ed03324e9
Adeonir Kohl <adeonir@gmail.com>
2020-09-05 01:26:05 -0300

    Create repository plugin

---
+++ plugins/repositories/admin/src/containers/HomePage/index.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import React, { memo, useState, useEffect } from "react";
+import { Header } from "@buffetjs/custom";
+import { Table } from "@buffetjs/core";
+
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding: 18px 30px;
+
+  p {
+    margin-top: 1rem;
+  }
+`;
+
+const HomePage = () => {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/adeonir/repos")
+      .then((res) => setRows(res.data))
+      .catch((e) => strapi.notification.error(`Ops...Github API error, ${e}`));
+  }, []);
+
+  const headers = [
+    {
+      name: "Name",
+      value: "name",
+    },
+    {
+      name: "Description",
+      value: "description",
+    },
+    {
+      name: "Url",
+      value: "html_url",
+    },
+  ];
+
+  return (
+    <Wrapper>
+      <Header
+        title={{ label: "React Avançado Repositories" }}
+        content="A list of repositories in React Avançado course."
+      />
+      <Table headers={headers} rows={rows} />
+    </Wrapper>
+  );
+};
+
+export default memo(HomePage);

c906cd75a4a7ab3080d9b3e4de6e32bbeff6a689
Adeonir Kohl <adeonir@gmail.com>
2020-09-05 16:19:49 -0300

    Add scrapper to populate the database

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: async (ctx) => {
+    console.info("Starting to populate...");
+
+    const options = {
+      sort: "popularity",
+      page: "1",
+      ...ctx.query,
+    };
+
+    await strapi.services.game.populate(options);
+    ctx.send({ status: 200 });
+
+    console.info("Finished populating!");
+  },
+};
